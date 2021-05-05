fdfd5524ecb2475a62b01a5c93f8255b150d603b
Vicente A <valcalde@miuandes.cl>
2021-05-05 15:05:16 -0400

    habia olvidado un ;

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 
 const view = countN =>{
- return (`\t count: ${countN}\n\t(+)(-) \n\t(q) for quit\n What would you do?`);
+ return (`\tcount: ${countN}\n\t(+)(-) \n\t(q) for quit\n What would you do?`);
     }  
 
 const update = (oldValue,msg)=>{
@@ -12,13 +12,13 @@ const update = (oldValue,msg)=>{
         else return oldValue; 
     }}
 let prompt = require('prompt-sync')();
-   
+console.clear();  
 //let n = prompt('');
 const app = counter =>{
     console.clear();
     console.log(view(counter));
     let answer = prompt('');
-    if (answer === "q") return
+    if (answer === "q") return;
     app(update(counter,answer));
 }
 
